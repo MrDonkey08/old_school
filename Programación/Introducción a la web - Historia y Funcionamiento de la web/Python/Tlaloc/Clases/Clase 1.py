43a9c8d9d4283db9d1194fb4bc3bb42f0bfe3b15
MrDonkey08 <113253976+MrDonkey08@users.noreply.github.com>
2023-03-15 07:43:02 -0600

    Created file: Programación/Introducción a la web - Historia y Funcionamiento de la web/Python/Tlaloc/Clases/Clase 1.py

---
+++ Programación/Introducción a la web - Historia y Funcionamiento de la web/Python/Tlaloc/Clases/Clase 1.py
@@ -0,0 +1,107 @@
+def Ejemplo_1():
+    omar = 0;
+    #nombre_de_la_lista.append(lo que quieres meter a la lista);
+    lista = [];
+    lista.append(omar);
+    print(lista)
+
+def Ejemplo_2():
+    omar = 0;
+    fabiola = 20;
+    lista = [];
+    lista.append(omar);
+    lista.append(fabiola);
+    print(lista);
+    print(lista['''posición'''])
+
+def Ejemplo_3():
+    lista = [];
+    cont = 0;
+    while True:
+        lista.append(cont);
+        if cont == 20:
+                break;
+    cont +=1; #cont = cont+1
+    print(lista);
+
+def Ejemplo_4():
+    lista = [];
+    cont = 0;
+    while cont < 3:
+        lista.append(cont);
+        if cont == 20:
+                break;
+    cont +=1; #cont = cont+1
+    print(lista);
+    lista = [];
+    lista_2 = [];
+    valor = 0
+    lista.append(valor)
+
+def Ejemplo_5():
+    lista = [];
+    valor = 0;
+    while valor <= 3:
+    lista.append(valor);
+    	valor +=1;
+    valor -=1;
+    print(lista);
+    while valor >0:
+    	lista_2.append(valor);
+    	valor -=1;
+    print(lista_2);
+
+def Ejemplo_6():
+	import time;
+	lista = ["perraco", "\nChúpame el pito", "\nMamawevo", "\nPerrionis", "Te quiero"]
+	cont = 0;
+	tope = len(lista);
+	tope = len(lista[0]); #Cantidad de cáracteres del valor 1 
+	while True:
+		time.sleep(0.5);
+		print(lista[cont]);
+		cont +?1;
+		if cont == tope:
+			break;
+
+def Ejemplo_7():
+	cont = 500;
+	cont_en_texto = str(cont); #500
+	cont_nuevo = int(cont_en_texto)
+	total = cont + cont_nuevo;
+	print(total)
+
+def Ejemplo_8():
+	try:
+		edad = int(input("¿Cuántos años tienes?"))
+		edad_float = int(input("Dame tu edad: "))
+		edad_str = int(input("Dame tu edad: "))
+	except:
+		print("No digas mamadas meri yein");
+		return Ejemplo_8()
+
+	if cont == 20:
+	
+def Ejemplo_9():
+	lista = [];
+	años = int(input("Dame tu edad: "));
+	if 0 < años <40:
+		lista.append(años);
+	elif años >= 40:
+		print("No te quiero, perro");
+		return Ejemplo_9();
+	else:
+		print("Aún te puedo abortar");
+		return Ejemplo_9();
+
+def Ejemplo_10():
+	cont = 9;
+	cont_2 = 10;
+	impar = cont_2 % 2;
+	par = cont_2 %
+
+def Ejemplo_11():
+	cont_2 = 10;
+	impar = cont %
+	
+Ejemplo_(9)
